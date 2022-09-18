8b18eb5bf6457e71e42b76660230551c993c0305
atn337 <38930758+atn337@users.noreply.github.com>
2022-09-18 01:58:58 -0300

    Initial commit

---
+++ video.py
@@ -0,0 +1,19 @@
+# 2. Grave um vídeo;
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture("video.mp4")
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        cv2.imshow('frame',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
